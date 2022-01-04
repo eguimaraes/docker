66a6a1f163265b5a26be8504911e88275c6395a1
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-01-04 14:14:01 -0300

    Create wordpressconf.sh

---
+++ wordpressconf.sh
@@ -0,0 +1,8 @@
+#ver https://ubuntu.com/tutorials/install-and-configure-wordpress#4-configure-apache-for-wordpress
+nano /etc/apache2/sites-available/wordpress.conf
+service apache2 start
+a2ensite wordpress
+a2enmod rewrite
+service apache2 restart
+a2dissite 000-default
+service apache2 reload
